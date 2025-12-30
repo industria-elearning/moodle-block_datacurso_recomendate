c67345d2ec3224eeb41290c8c6e5fd145bebc062
JosueCH-17 <josue@datacurso.com>
2025-12-30 15:28:51 -0500

    fix update release

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_datacurso_recomendate';
-$plugin->release = '1.0.3';
-$plugin->version = 2025120500;
+$plugin->release = '1.0.4';
+$plugin->version = 2025123000;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 501];
